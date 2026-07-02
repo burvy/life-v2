900c44e4d8091d839a9985ef0f949bc357046308
Burvye <thebuffonion@gmail.com>
2026-07-02 17:39:42 -0400

    fix: attempt to fix runtime crash

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 mod window;
-use window::{norm, App};
+use window::App;
 use winit::event_loop::EventLoop;
 
 fn main() {
+    env_logger::init();
     println!("Hello, World!");
     let event_loop = EventLoop::new().expect("ur stupid event loop didnt even start correctly");
     let mut app = App {
-        bg_clr: [norm(206), norm(66), norm(43)],
-        ..Default::default() // this is to handle the window which works with just default
+        ..Default::default()
     };
     let _ = event_loop.run_app(&mut app);
 }
--- src/window.rs
+++ src/window.rs
@@ -10,6 +10,19 @@ use winit::{
     window::{Window, WindowId},
 };
 
+pub struct Graphics {
+    /// just make this the default
+    /// window is window
+    /// WINDOW SSISS NIDOASDNA OSIDNALSD
+    pub window: Arc<Window>,
+    /// put ur pixels here
+    /// also compiler rlly wants this to have a lifetime so wtv
+    pub pixels: Pixels<'static>,
+
+    /// RGB without the alpha to see if it doesnt crash
+    pub bg_clr: [f64; 3],
+}
+
 
 /// now all of T S boiler plate has to be written
 /// bc winit decided it must be so
@@ -19,31 +32,23 @@ use winit::{
 /// also the window lives always
 #[derive(Default)]
 pub struct App {
-    /// just make this the default
-    /// window is window
-    /// WINDOW SSISS NIDOASDNA OSIDNALSD
-    pub window: Option<Arc<Window>>, // whyyy does this need to be public
-    /// put ur pixels here
-    /// also compiler rlly wants this to have a lifetime so wtv
-    pub pixels: Option<Pixels<'static>>,
-    /// RGB without the alpha to see if it doesnt crash
-    pub bg_clr: [f64; 3],
+    pub graphics: Option<Graphics>,
 }
 
 impl ApplicationHandler for App {
     /// create a new window if there is no window already
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        if self.window.is_none() {
+        if self.graphics.is_none() {
             let window_attributes = Window::default_attributes()
                 .with_title("Cellular Automata") // this is probably how its spelled
                 // no transparency bc it might crash
                 .with_inner_size(LogicalSize::new(800, 600)); // do some more method shopping if u want
             // window is an arc which makes things so much better
             let window = Arc::new(event_loop.create_window(window_attributes).unwrap());
-            self.window = Some(window.clone());
-            // so we can solve borrow checker issues by just cloning window earlier
             let size = window.inner_size();
-            let surface_texture = SurfaceTexture::new(size.width, size.height, window);
+            println!("created pixels with size {}x{}", size.width, size.height);
+            // so we can solve borrow checker issues by just cloning window earlier
+            let surface_texture = SurfaceTexture::new(size.width, size.height, window.clone());
             // the alternative to not using pollster is horrific so we should use it
             let pixels = pollster::block_on(
                 async {
@@ -51,7 +56,10 @@ impl ApplicationHandler for App {
                     Pixels::new_async(size.width, size.height, surface_texture).await
                 }
             ).expect("couldn't create pixels");
-            self.pixels = Some(pixels); // TODO: just make this work
+            self.graphics = Some(Graphics { window, pixels, bg_clr: [0.0, 0.0, 0.0] });
+            // try rendering immediately
+            self.graphics.as_mut().unwrap().pixels.render().expect("initial render failed");
+            self.graphics.as_ref().unwrap().window.request_redraw();
         }
     }
     /// detect events and run actions on them if you want
@@ -61,38 +69,45 @@ impl ApplicationHandler for App {
         window_id: WindowId, // why do i even need window id what is this for
         event: WindowEvent,
     ) {
-        dbg!("{} happened to window {}", &event, &window_id); // can we do something with window_id
-        let window = self.window.as_ref().expect("ur window doesnt exist");
-        let Some(pixels) = self.pixels.as_mut() else {
-            dbg!("self pixels could not be did as mut");
-            return;
-        };
-        match event {
-            WindowEvent::Resized(size) => {
-                if let Some(pixels) = self.pixels.as_mut() {
-                    pixels.resize_surface(size.width, size.height).expect("couldn't resize surface");
-                }
-                if let Some(window) = self.window.as_ref() {
-                    window.request_redraw();
+        if let WindowEvent::Resized(size) = &event {
+            if size.width > 0 && size.height > 0 {
+                if let Some(graphics) = self.graphics.as_mut() {
+                    let surface = SurfaceTexture::new(size.width, size.height, graphics.window.clone());
+                    graphics.pixels = pollster::block_on(
+                        Pixels::new_async(size.width, size.height, surface)
+                    ).expect("couldn't recreate pixels on resize");
+                    graphics.window.request_redraw();
                 }
             }
+            return;
+        }
+        println!("{:?} happened to window {:?}", &event, &window_id); // can we do something with window_id
+        let Some(graphics) = self.graphics.as_mut() else { return; };
+        match event {
             WindowEvent::CloseRequested => {
                 event_loop.exit();
             }
             WindowEvent::CursorEntered { .. } => {
-                self.bg_clr = [norm(206), norm(66), norm(43)]; // this is ferris rust orange btw
-                window.request_redraw();
+                graphics.bg_clr = [norm(206), norm(66), norm(43)];
+                graphics.window.request_redraw();
             }
             WindowEvent::CursorLeft { .. } => {
-                self.bg_clr = [norm(43), norm(66), norm(206)]; // this would be the OPPOSITE of ferris orange
-                window.request_redraw();
+                graphics.bg_clr = [norm(43), norm(66), norm(206)]; // this would be the OPPOSITE of ferris orange
+                graphics.window.request_redraw();
             }
             WindowEvent::RedrawRequested => {
-                    let [r, g, b,] = self.bg_clr;
 
-                    pixels.clear_color(Color { r, g, b, a: 1.0 });
+                    let window_size = graphics.window.inner_size();
+                    let surface_size = graphics.pixels.texture().size();
+                    if window_size.width != surface_size.width || window_size.height != surface_size.height {
+                        println!("size mismatch, skipping render");
+                        return;
+                    }
+                    let [r, g, b,] = graphics.bg_clr;
 
-                    if let Err(err) = pixels.render() {
+                    graphics.pixels.clear_color(Color { r, g, b, a: 1.0 });
+
+                    if let Err(err) = graphics.pixels.render() {
                         println!("couldnt render pixels bc of error: {err}");
                         event_loop.exit();
                     }
